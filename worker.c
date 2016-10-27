f1762cefe25dc0fc0f28969d16314ab75a9416af
GuilhermeMenezes <minuzes@hotmail.com>
2016-10-27 14:47:03 +0100

    UPDATE: prints apagados?

--- worker.c
+++ worker.c
@@ -7,21 +7,21 @@ void *worker(void *arg){
 
 	while(1){
 		
-		printf("WWWWWWWWWWWWWWWWWWWW1\n");
+		
 		comando_t executa;
 		executa = buff_pop();
 
 		printf("%d %d\n", executa.idConta, executa.valor);
 
 		if (executa.operacao == OP_DEBITAR){
-			printf("ENTREI NO DEBITAR\n");
+			
 			if (debitar(executa.idConta, executa.valor) < 0)
 				printf("%s(%d, %d): Erro\n\n", COMANDO_DEBITAR, executa.idConta, executa.valor);
 			else
 				printf("%s(%d, %d): OK\n\n", COMANDO_DEBITAR, executa.idConta, executa.valor);
 		}
 		if (executa.operacao == OP_CREDITAR){
-			printf("ENTREI NO CREDITAR\n");
+			
 			if (creditar(executa.idConta, executa.valor) < 0)
 				printf("%s(%d, %d): Erro\n\n", COMANDO_CREDITAR, executa.idConta, executa.valor);
 			else
@@ -29,7 +29,7 @@ void *worker(void *arg){
 			
 		}
 		if (executa.operacao == OP_LERSALDO){
-			printf("ENTREI NO LERSALDO\n");
+			
 			if (lerSaldo(executa.idConta) < 0)
 				printf("%s(%d): Erro.\n\n", COMANDO_LER_SALDO, lerSaldo(executa.idConta));
 			else
